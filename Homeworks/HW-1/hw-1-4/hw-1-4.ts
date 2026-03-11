a6c6a9b53c9f29437b7da4aba9f3e33d0aaf5027
cat007power-art <cat007power@gmail.com>
2026-03-11 15:28:44 +0300

    завершено

---
+++ Homeworks/HW-1/hw-1-4/hw-1-4.ts
@@ -0,0 +1,15 @@
+
+type User_1 = {name: string, username: string, password: string};
+
+let user4:User_1= {name: 'aaaaa', username: 'aaa1', password: '1234'};
+let user5:User_1= {name: 'aaaaa', username: 'aaa1', password: '1234'};
+let user6:User_1= {name: 'aaaaa', username: 'aaa1', password: '1234'};
+
+let users_task_1_4: User_1[]=[
+    user4,
+    {name: 'bbbbb', username: 'bbb', password: '1234'},
+user5,
+    {name: 'ccccc', username: 'ccc', password: '1234'},
+user6,
+    {name: 'ddddd', username: 'ddd', password: '1234'},
+];
