4d6a7a0f102894f6818afa1734739dd814f23761
cat007power-art <cat007power@gmail.com>
2026-03-12 13:37:43 +0300

    завершено

---
+++ Homeworks/HW-1/hw-1-7/hw-1-7.ts
@@ -0,0 +1,10 @@
+let time:number = 49;
+if (time <= 0 && time < 15) {
+    console.log('1 quarter');
+}else if (time <= 15 && time < 30) {
+    console.log('2 quarter');
+}else if (time <= 30 && time < 45) {
+    console.log('3 quarter');
+}else if (time <= 45 && time < 60) {
+    console.log('4 quarter');
+}
