98a0121eb32d1bb9cbf66a61c280c1c2d20c6831
cat007power-art <cat007power@gmail.com>
2026-03-12 14:38:59 +0300

    завершено

---
+++ Homeworks/HW-1-1/hw-1/hw-5.ts
@@ -0,0 +1,7 @@
+let listOfItems: string[] = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+document.write('<ul>');
+for (let i = 0; i < listOfItems.length; i++) {
+    document.write(`<li>${listOfItems[i]}</li>`);
+}
+document.write('</ul>');
+
