66c5e271d3edf85ed3ba5f0eda3289b4af96b3c7
cat007power-art <cat007power@gmail.com>
2026-03-12 13:49:39 +0300

    завершено

---
+++ Homeworks/HW-1/hw-1-9/hw-1-9.ts
@@ -0,0 +1,52 @@
+
+let userKey:string|null = prompt()
+if (userKey !== null) {
+    let key:number= +userKey;
+    switch (key) {
+        case 1:
+            console.log(abc);
+            console.log(abc);
+            console.log(abc);
+            console.log(abc);
+            break;
+        case 2:
+            console.log(111111);
+            console.log(111111);
+            console.log(111111);
+            console.log(111111);
+            console.log(111111);
+            break;
+        case 3:
+            console.log(22222);
+            console.log(22222);
+            console.log(22222);
+            console.log(22222);
+            break;
+        case 4:
+            console.log(33333);
+            console.log(33333);
+            console.log(33333);
+            console.log(33333);
+            break;
+        case 5:
+            console.log(44444);
+            console.log(44444);
+            console.log(44444);
+            console.log(44444);
+            break;
+        case 6:
+            console.log(55555);
+            console.log(55555);
+            console.log(55555);
+            console.log(55555);
+            break;
+        case 7:
+            console.log(66666);
+            console.log(66666);
+            console.log(66666);
+            console.log(66666);
+            break;
+        default:
+            console.log('?????');
+    }
+}
