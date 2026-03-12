48b892d698dc42c117d66f763c1f9bce81c0e6b4
cat007power-art <cat007power@gmail.com>
2026-03-12 13:19:55 +0300

    завершено

---
+++ Homeworks/HW-1/hw-1-5/hw-1-5.ts
@@ -0,0 +1,11 @@
+
+type Temperature = {day: string,morningTemp: number, dayTemp: number, eveningTemp: number };
+const temps: Temperature[] = [
+    {day: 'monday', morningTemp:8,dayTemp:15, eveningTemp:11},
+    {day: 'tuesday', morningTemp:10,dayTemp:17, eveningTemp:13},
+    {day: 'wednesday', morningTemp:15,dayTemp:24, eveningTemp:18},
+    {day: 'thursday', morningTemp:14,dayTemp:20, eveningTemp:17},
+    {day: 'friday', morningTemp:20,dayTemp:26, eveningTemp:24},
+    {day: 'saturday', morningTemp:15,dayTemp:20, eveningTemp:14},
+    {day: 'sunday', morningTemp:1,dayTemp:24, eveningTemp:10},
+]
