10d69acd35588efc55f19d8d3844c4f0eb3dc3b0
cat007power-art <cat007power@gmail.com>
2026-03-13 15:34:22 +0300

    завершено

---
+++ Homeworks/HW-1-2/hw-6.ts
@@ -0,0 +1,9 @@
+function createList(text: string, counter: number):void {
+    document.write('<ul>');
+    for (let i = 0; i < counter; i++) {
+        document.write(`<li>${text}</li>`);
+    }
+    document.write('</ul>');
+}
+
+createList ('hello',5);
