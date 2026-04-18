3be77d6b8f68b6199c170366120ce19309b539d5
cat007power-art <cat007power@gmail.com>
2026-04-18 17:50:12 +0300

    завершено

---
+++ Homeworks/HW-1-4/hw-1-3.ts
@@ -0,0 +1,11 @@
+let s: string = 'HELLO WORLD';
+const s1: string = s.toLowerCase();
+console.log(s1);
+
+s = 'LOREM IPSUM';
+const s2: string = s.toLowerCase();
+console.log(s2);
+
+s = 'JAVASCRIPT IS COOL';
+const s3: string = s.toLowerCase();
+console.log(s3);
